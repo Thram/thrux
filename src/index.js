bd39202093ebba7c5c771eb2a7be2c2961eb2c9e
Thram <thramposo@gmail.com>
2017-01-17 22:48:54 +1300

    - First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by thram on 16/01/17.
+ */
+
+function test() {
+ console.log('This is a test function')
+}
+
+module.exports = {
+  test: test,
+};
---
+++ src/store.js
@@ -0,0 +1,3 @@
+/**
+ * Created by thram on 16/01/17.
+ */
