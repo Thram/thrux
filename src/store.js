3987401fd152fb116a3c4e140ab22acbcc2f445d
Thram <thramposo@gmail.com>
2017-02-04 17:56:14 +1300

    - Add Licence a

--- src/store.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by thram on 16/01/17.
- */
-let store = {};
-
-export const getState = (key) => key ? store[key] : store;
-export const setState = (key, value) => store[key] = value;
