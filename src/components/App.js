7d56a1e15b2cf1e723f308edec018b29a5d10383
Thram <thramposo@gmail.com>
2017-02-19 19:44:06 +1300

    - improve Menu button

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
  * Created by thram on 21/01/17.
  */
 import React, {Component} from "react";
-import {debounce} from "lodash";
+import {debounce, times} from "lodash";
 import {connect} from "react-thrux";
 import {observe} from "thrux";
 import Markdown from "./Markdown";
@@ -11,19 +11,19 @@ import Menu from "./Menu";
 class App extends Component {
   state = {menuOpen: false};
 
-  openMenu = () => this.setState({menuOpen: true});
-
-  goHome = () => this.state.menuOpen ? this.setState({menuOpen: false}): location.hash='#';
+  toggleMenu = () => this.setState({menuOpen: !this.state.menuOpen});
 
   componentDidMount = () => observe('route', () => this.setState({menuOpen: false}));
 
   render = () => (
       <div className="container">
-        <a onClick={this.openMenu} className="menu-logo" style={this.state.menuOpen ? {display: 'none'} : {}}>
-          <img src="https://raw.githubusercontent.com/Thram/thrux/gh-pages/assets/thrux_icon.png" alt="Menu"/>
+        <a onClick={this.toggleMenu} className="menu-btn" >
+          <div className={`nav-icon ${this.state.menuOpen && 'open'}`}>
+            {times(4, (index) => <span key={index}/>)}
+          </div>
         </a>
         <div className="side-bar" style={this.state.menuOpen ? {display: 'block'} : {}}>
-          <a onClick={this.goHome} className="logo">
+          <a href="#" className="logo">
             <img src="https://raw.githubusercontent.com/Thram/thrux/master/thrux_logo.png" alt="Thrux"/>
           </a>
           <Menu />
