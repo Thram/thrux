9bf653daa315e210d19cec187931a53026608736
Thram <thramposo@gmail.com>
2017-02-19 17:54:54 +1300

    - Starting with responsive stuff

--- src/components/App.js
+++ src/components/App.js
@@ -4,52 +4,27 @@
 import React, {Component} from "react";
 import {debounce} from "lodash";
 import {connect} from "react-thrux";
-import {register, createDict, dispatch} from "thrux";
 import Markdown from "./Markdown";
 import Menu from "./Menu";
 
 class App extends Component {
   render = () => (
-    <div className="container">
-      <a href="https://github.com/Thram/thrux" className="github-logo">
-        <img src="https://raw.githubusercontent.com/Thram/thrux/assets/GitHub-Mark-120px-plus.png" alt="Github"/>
-      </a>
-      <div className="side-bar" style={this.state.app.sideBar}>
-        <a href="#" className="logo">
-          <img src="https://raw.githubusercontent.com/Thram/thrux/master/thrux_logo_sm.png" alt="Thrux"/>
-        </a>
-        <Menu />
+      <div className="container">
+        <div className="side-bar">
+          <a href="#" className="logo">
+            <img src="https://raw.githubusercontent.com/Thram/thrux/master/thrux_logo_sm.png" alt="Thrux"/>
+          </a>
+          <Menu />
+          <a href="https://github.com/Thram/thrux" className="github-logo">
+            <img src="https://raw.githubusercontent.com/Thram/thrux/gh-pages/assets/GitHub-Mark-Light-120px-plus.png"
+                 alt="Github"/>
+          </a>
+        </div>
+        <div className="content">
+          <Markdown page={this.state.route}/>
+        </div>
       </div>
-      <div className="content" style={this.state.app.content}>
-        <Markdown page={this.state.route}/>
-      </div>
-    </div>
   )
 }
 
-const SIDE_BAR_WIDTH = 200,
-      SIDE_BAR_LIMIT = .4;
-
-const updateStyles = createDict(() => {
-  const sideBarRatio = 200 / document.body.clientWidth;
-  return {
-    sideBar: {
-      overflow: 'hidden',
-      width   : sideBarRatio < SIDE_BAR_LIMIT ? `${SIDE_BAR_WIDTH}px` : '0'
-    },
-    content: {
-      width: sideBarRatio < SIDE_BAR_LIMIT ? `${document.body.clientWidth - SIDE_BAR_WIDTH}px` : '100%'
-    }
-  };
-});
-
-register({
-  app: {
-    INIT         : updateStyles,
-    UPDATE_STYLES: updateStyles
-  }
-});
-
-window.addEventListener("resize", debounce(() => dispatch('app:UPDATE_STYLES'), 150));
-
-export default connect(['route', 'app'], App);
+export default connect(['route'], App);
