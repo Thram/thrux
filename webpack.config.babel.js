56a77fafd1a58043e28dffb0f035ffc7818088a5
Thram <thramposo@gmail.com>
2017-03-24 03:41:48 +1300

    fix(webpack): ESLint webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,21 +1,21 @@
 /**
  * Created by thram on 18/01/17.
  */
-import {join} from 'path';
+import { join } from 'path';
 
 const include = join(__dirname, 'src');
 
 export default {
-  entry  : './src/index',
-  output : {
-    path         : join(__dirname, 'dist'),
+  entry: './src/index',
+  output: {
+    path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library      : 'thrux',
+    library: 'thrux',
   },
   devtool: 'source-map',
-  module : {
+  module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel-loader', include}
-    ]
-  }
+      { test: /\.js$/, loader: 'babel-loader', include },
+    ],
+  },
 };
